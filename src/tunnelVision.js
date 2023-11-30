971d1350cebfd752af3eb938e40da46dd5d9fa78
Ali Rizvi <alirizvi139@gmail.com>
2023-11-30 17:09:11 +0500

    deploy: compress assets

--- src/tunnelVision.js
+++ src/tunnelVision.js
@@ -6,59 +6,59 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 //===================================================== SOCIAL MEDIA
 const Box1T =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Social/2.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Social/webp/2.webp";
 const Box2T =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Social/3.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Social/webp/3.webp";
 const Box3T =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Social/6.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Social/webp/6.webp";
 const Box4T =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Social/7.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Social/webp/7.webp";
 
 //===================================================== PHOTOGRAPHY
 const BoxP1 =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Photography/10.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Photography/webp/10.webp";
 const BoxP2 =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Photography/18.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Photography/webp/18.webp";
 const BoxP3 =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Photography/20.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Photography/webp/20.webp";
 const BoxP4 =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Photography/23.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Photography/webp/23.webp";
 const BoxP5 =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Photography/28.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Photography/webp/28.webp";
 const BoxP6 =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Photography/34.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Photography/webp/34.webp";
 
 //===================================================== BRANDING
 const Box1B =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Branding/1.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Branding/webp/1.webp";
 const Box2B =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Branding/3.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Branding/webp/3.webp";
 const Box3B =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Branding/4.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Branding/webp/4.webp";
 const Box4B =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Branding/5.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Branding/webp/5.webp";
 const Box5B =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Branding/6.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Branding/webp/6.webp";
 const Box6B =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/Branding/8.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/Branding/webp/8.webp";
 
 //===================================================== UIUX
 const Box1U =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/UIUX/1.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/UIUX/webp/1.webp";
 const Box2U =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/UIUX/2.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/UIUX/webp/2.webp";
 const Box3U =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/UIUX/3.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/UIUX/webp/3.webp";
 const Box4U =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/UIUX/4.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/UIUX/webp/4.webp";
 const Box5U =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/UIUX/5.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/UIUX/webp/5.webp";
 const Box6U =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/UIUX/6.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/UIUX/webp/6.webp";
 const Box7U =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/UIUX/7.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/UIUX/webp/7.webp";
 const Box8U =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/UIUX/8.png";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/UIUX/webp/8.webp";
 //===================================================== Shaders
 const vertexShader = `
   varying float vDistance;
@@ -483,7 +483,7 @@ scene.add(boxU1, boxU2, boxU3, boxU4, boxU5, boxU6, boxU7, boxU8);
 var videoV1 = document.createElement("video");
 var videoTextureV1 = new THREE.VideoTexture(videoV1);
 videoV1.src =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/1.mp4";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/videos/1.mp4";
 videoV1.crossOrigin = "anonymous";
 videoV1.muted = "muted";
 videoV1.loop = true;
@@ -505,7 +505,7 @@ boxV1.position.set(-0.4, -0.2, 74);
 var videoV2 = document.createElement("video");
 var videoTextureV2 = new THREE.VideoTexture(videoV2);
 videoV2.src =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/2.mp4";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/videos/2.mp4";
 videoV2.crossOrigin = "anonymous";
 videoV2.muted = "muted";
 videoV2.loop = true;
@@ -526,7 +526,7 @@ boxV2.position.set(0.4, -0.2, 73);
 var videoV3 = document.createElement("video");
 var videoTextureV3 = new THREE.VideoTexture(videoV3);
 videoV3.src =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/3.mp4";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/videos/3.mp4";
 videoV3.crossOrigin = "anonymous";
 videoV3.muted = "muted";
 videoV3.loop = true;
@@ -547,7 +547,7 @@ boxV3.position.set(-0.4, 0.4, 72);
 var videoV4 = document.createElement("video");
 var videoTextureV4 = new THREE.VideoTexture(videoV4);
 videoV4.src =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/4.mp4";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/videos/4.mp4";
 videoV4.crossOrigin = "anonymous";
 videoV4.muted = "muted";
 videoV4.loop = true;
@@ -568,7 +568,7 @@ boxV4.position.set(0.4, 0.4, 71);
 var videoV5 = document.createElement("video");
 var videoTextureV5 = new THREE.VideoTexture(videoV5);
 videoV5.src =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/5.mp4";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/videos/5.mp4";
 videoV5.crossOrigin = "anonymous";
 videoV5.muted = "muted";
 videoV5.loop = true;
@@ -590,7 +590,7 @@ boxV5.position.set(-0.4, -0.2, 70);
 var videoV6 = document.createElement("video");
 var videoTextureV6 = new THREE.VideoTexture(videoV6);
 videoV6.src =
-  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.2/src/assets/2.mp4";
+  "https://cdn.jsdelivr.net/gh/mgohar/dapTunnelAnim@0.1.8/src/assets/videos/2.mp4";
 videoV6.crossOrigin = "anonymous";
 videoV6.muted = "muted";
 videoV6.loop = true;
